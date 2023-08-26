cce119ee3aeb14803af843a4febd1f2a64dcc788
Silke Hofmann <silke@silkehofmann.eu>
2023-08-26 17:44:13 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Silke Hofmann on 26.08.23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
