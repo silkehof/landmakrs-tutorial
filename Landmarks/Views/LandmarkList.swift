a9f58e04fe315ade23a0659fc33ef7d2c950a2a9
Silke Hofmann <silke@silkehofmann.eu>
2023-08-27 01:08:22 +0200

    Create LandmarkList to display list of LandmarkRows

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Silke Hofmann on 27.08.23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
