e5b3a5b4fa0f3aa749d9ad05ec4d06a81c3891c1
Silke Hofmann <silke@silkehofmann.eu>
2023-08-30 00:31:43 +0200

    Update LandmarkList preview to be displayed on several different devices

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -24,6 +24,10 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone 14 Pro", "iPad Pro (12.9-inch) (6th generation)"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
